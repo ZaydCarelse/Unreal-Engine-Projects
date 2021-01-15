339165b3a13da0a865b376a2ef2c4ce80bdf0591
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-15 15:02:13 +0200

    Second after lunch commit

--- TripleXBike/TripleXBike.cpp
+++ TripleXBike/TripleXBike.cpp
@@ -5,5 +5,17 @@ int main()
     std:: cout << "You are a bike thief, trying to crack the lock of your next bike.";
     std:: cout << std::endl;
     std:: cout << "You need to enter the correct combination to continue...";
+
+
+    int a = 7;
+    int b = 2;
+    int c = 5;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << "The sum of your number is: " << sum << std::endl;
+    std::cout << "The product of your number is: " << product << std::endl;
     return 0;
 }
