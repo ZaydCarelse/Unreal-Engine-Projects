d8021fca63f83ac9c8bf4684c842d8af5b2e88b6
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-15 22:17:32 +0200

    Finished Input and Conditions for Win and Loss

--- TripleXBike/TripleXBike.cpp
+++ TripleXBike/TripleXBike.cpp
@@ -23,7 +23,37 @@ int main() //Main function of the application
     std::cout << "+ The combination lock digits add up to: " << CodeSum << std::endl;
     std::cout << "+ The product of the digits is: " << CodeProduct << std::endl;
 
-    int PlayerGuess;
-    
+    int GuessA, GuessB, GuessC;
+
+    std::cout << std::endl;
+    std::cout << "What do you think the first number is? ";
+    std::cin >> GuessA;
+    std::cout << std::endl;
+    std::cout << "- You fumble around with the lock and enter the number " << GuessA << ". Two more to go. -" << std::endl;
+    std::cout << std::endl;
+    std::cout << "What do you think the second number is? ";
+    std::cin >> GuessB;
+    std::cout << std::endl;
+    std::cout << "- With a twist of the second digit, you pick " << GuessB << ". One more to go. -" << std::endl;
+    std::cout << std::endl;
+    std::cout << "What do you think the last number is? ";
+    std::cin >> GuessC;
+    std::cout << std::endl;
+    std::cout << "- This is it...almost there. You pick " << GuessC << " as the last number. -" << std::endl;
+
+    int GuessSum = GuessA + GuessB + GuessB;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct){
+        std::cout << std::endl;
+        std::cout << "Your hands yank on the lock and...it UNLOCKS!" << std::endl << std::endl;
+        std::cout << "*** HEY YOU! STOP! You grab the bike and ride away before you're caught. Whew! ***" << std::endl;
+    } else {
+        std::cout << std::endl;
+        std::cout << "Your hands yank on the lock and...it stays locked!" << std::endl << std::endl;
+        std::cout << "***               HEY YOU! STOP!                   ***" << std::endl << std::endl;
+        std::cout << "*** PUT YOUR HANDS UP! YOU'RE GOING TO JAIL, PUNK! ***" << std::endl;
+    }
+
     return 0;
 }
