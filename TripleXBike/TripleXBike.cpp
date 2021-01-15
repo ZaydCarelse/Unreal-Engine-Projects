cd5fe34d792d8f4a0044de34fc7d489617aab157
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-15 14:23:22 +0200

    Commit after lunch 15/01/2021

--- TripleXBike/TripleXBike.cpp
+++ TripleXBike/TripleXBike.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    std:: cout << "You are a bike thief, trying to crack the lock of your next bike.";
+    std:: cout << std::endl;
+    std:: cout << "You need to enter the correct combination to continue...";
     return 0;
 }
