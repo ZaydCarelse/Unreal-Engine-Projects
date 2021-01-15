0c6053699fe7647c3f54f1421a1ab570e288040a
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-15 22:38:24 +0200

    Added escape sequences and moved the body of the game into the PlayGame function

--- TripleXBike/TripleXBike.cpp
+++ TripleXBike/TripleXBike.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-int main() //Main function of the application
+void PlayGame()
 {
-    
     //This expression statement introduces the player to the game
-    std:: cout << "*********************************************************************" << std::endl;
-    std:: cout << "* You are a bike thief, trying to crack the lock of your next bike. *" << std::endl;
-    std:: cout << "* You need to enter the correct combination to unlock the bike...   *" << std::endl;
-    std:: cout << "* If you fail to open the lock, you'll end up in jail...            *" << std::endl;
-    std:: cout << "*********************************************************************" << std::endl;
+    std:: cout << "*********************************************************************\n";
+    std:: cout << "* You are a bike thief, trying to crack the lock of your next bike. *\n";
+    std:: cout << "* You need to enter the correct combination to unlock the bike...   *\n";
+    std:: cout << "* If you fail to open the lock, you'll end up in jail...            *\n";
+    std:: cout << "*********************************************************************\n";
 
     //This is a declaration statement in which we declare variables
     int CodeA = 7;
@@ -18,42 +17,39 @@ int main() //Main function of the application
     int CodeSum = CodeA + CodeB + CodeB;
     int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << "+ The bike that you've chosen to steal is tied to a lamp-post." << std::endl;
-    std::cout << "+ The combination lock digits add up to: " << CodeSum << std::endl;
-    std::cout << "+ The product of the digits is: " << CodeProduct << std::endl;
+    std::cout << "\n+ The bike that you've chosen to steal is tied to a lamp-post.";
+    std::cout << "\n+ The combination lock digits add up to: " << CodeSum;
+    std::cout << "\n+ The product of the digits is: " << CodeProduct;
 
     int GuessA, GuessB, GuessC;
 
-    std::cout << std::endl;
-    std::cout << "What do you think the first number is? ";
+    //Asking the player for their input
+    std::cout << "\n\nWhat do you think the first number is? ";
     std::cin >> GuessA;
-    std::cout << std::endl;
-    std::cout << "- You fumble around with the lock and enter the number " << GuessA << ". Two more to go. -" << std::endl;
-    std::cout << std::endl;
-    std::cout << "What do you think the second number is? ";
+    std::cout << "\n- You fumble around with the lock and enter the number " << GuessA << ". Two more to go. -";
+    std::cout << "\n\nWhat do you think the second number is? ";
     std::cin >> GuessB;
-    std::cout << std::endl;
-    std::cout << "- With a twist of the second digit, you pick " << GuessB << ". One more to go. -" << std::endl;
-    std::cout << std::endl;
-    std::cout << "What do you think the last number is? ";
+    std::cout << "\n- With a twist of the second digit, you pick " << GuessB << ". One more to go. -\n";
+    std::cout << "\nWhat do you think the last number is? ";
     std::cin >> GuessC;
-    std::cout << std::endl;
-    std::cout << "- This is it...almost there. You pick " << GuessC << " as the last number. -" << std::endl;
+    std::cout << "\n- This is it...almost there. You pick " << GuessC << " as the last number. -\n";
 
     int GuessSum = GuessA + GuessB + GuessB;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    //Checks that the players input is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct){
-        std::cout << std::endl;
-        std::cout << "Your hands yank on the lock and...it UNLOCKS!" << std::endl << std::endl;
-        std::cout << "*** HEY YOU! STOP! You grab the bike and ride away before you're caught. Whew! ***" << std::endl;
-    } else {
-        std::cout << std::endl;
-        std::cout << "Your hands yank on the lock and...it stays locked!" << std::endl << std::endl;
-        std::cout << "***               HEY YOU! STOP!                   ***" << std::endl << std::endl;
-        std::cout << "*** PUT YOUR HANDS UP! YOU'RE GOING TO JAIL, PUNK! ***" << std::endl;
+        std::cout << "\nYour hands yank on the lock and...it UNLOCKS!\n\n";
+        std::cout << "*** HEY YOU! STOP! You grab the bike and ride away before you're caught. Whew! ***\n";
+    } else { //or incorrect
+        std::cout << "\nYour hands yank on the lock and...it stays locked!\n\n";
+        std::cout << "***               HEY YOU! STOP!                   ***\n\n";
+        std::cout << "*** PUT YOUR HANDS UP! YOU'RE GOING TO JAIL, PUNK! ***\n";
     }
+}
 
+int main() //Main function of the application
+{
+    PlayGame();
     return 0;
 }
