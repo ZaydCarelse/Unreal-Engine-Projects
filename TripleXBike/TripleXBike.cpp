d0dd58daadd41cb83ec629f9fa828d06d4dd1327
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-16 00:08:35 +0200

    Game Complete

--- TripleXBike/TripleXBike.cpp
+++ TripleXBike/TripleXBike.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <ctime>
 
-void PlayGame()
+void PrintIntroduction(int Difficulty)
 {
-    //This expression statement introduces the player to the game
-    std:: cout << "*********************************************************************\n";
+     //This expression statement introduces the player to the game
+    std:: cout << "\n\n*********************************************************************\n";
     std:: cout << "* You are a bike thief, trying to crack the lock of your next bike. *\n";
     std:: cout << "* You need to enter the correct combination to unlock the bike...   *\n";
     std:: cout << "* If you fail to open the lock, you'll end up in jail...            *\n";
+    std:: cout << "* The bike has a level " << Difficulty << " lock.                                      *\n";
     std:: cout << "*********************************************************************\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
 
     //This is a declaration statement in which we declare variables
-    int CodeA = 7;
-    int CodeB = 2;
-    int CodeC = 5;
+    int CodeA = rand() % Difficulty + Difficulty;
+    int CodeB = rand() % Difficulty + Difficulty;
+    int CodeC = rand() % Difficulty + Difficulty;
 
     int CodeSum = CodeA + CodeB + CodeB;
     int CodeProduct = CodeA * CodeB * CodeC;
@@ -41,15 +48,36 @@ void PlayGame()
     if (GuessSum == CodeSum && GuessProduct == CodeProduct){
         std::cout << "\nYour hands yank on the lock and...it UNLOCKS!\n\n";
         std::cout << "*** HEY YOU! STOP! You grab the bike and ride away before you're caught. Whew! ***\n";
+        return true;
     } else { //or incorrect
         std::cout << "\nYour hands yank on the lock and...it stays locked!\n\n";
         std::cout << "***               HEY YOU! STOP!                   ***\n\n";
         std::cout << "*** PUT YOUR HANDS UP! YOU'RE GOING TO JAIL, PUNK! ***\n";
+        return false;
     }
 }
 
 int main() //Main function of the application
 {
-    PlayGame();
+    srand(time(NULL)); //create a new random sequence based on the time of day
+    
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) //Loop until all level difficulties are played
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards the buffer
+
+        if (bLevelComplete)
+        {
+            //Increase the difficulty
+            ++LevelDifficulty;
+        } else {
+            return 0;
+        }
+    }
+    std::cout << "CONGRATULATIONS! You're a successful thief! Your parents must be so proud.";
     return 0;
 }
