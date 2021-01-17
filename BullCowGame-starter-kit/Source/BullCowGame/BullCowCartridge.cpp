176223c9c55f97519426ce1b930245497f7d48d1
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-17 17:45:02 +0200

    Pre-workout Commit

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to Bulls and Cows!");
+    PrintLine("Press Enter to continue...");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
